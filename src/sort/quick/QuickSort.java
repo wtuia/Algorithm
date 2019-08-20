ceb86a6f2c544d56e305f3f97efc491e4d0dcf54
yujiang <yujiang4j@qq.com>
2019-08-20 11:49:34 +0800

    Algo @version 1.0 ADD merge/quick sort

--- src/sort/quick/QuickSort.java
+++ src/sort/quick/QuickSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int[] a = {4,5,6,3,2,1,77,0,45,7};
+        int[] a = {4,5,6,3,1,8,7,3,2,5};
         sort(a, 0 ,a.length - 1);
         System.out.println(Arrays.toString(a));
     }
@@ -19,20 +19,30 @@ public class QuickSort {
     }
 
     private static int partition(int [] a, int start, int end) {
-        int pivot = a [end]; // 选取中间值,中间值不能包含在i的序列之间, 此处end不会被比较交换,不会打乱已排序的顺序
+        // 选取中间值,中间值不能包含在i的序列之间,
+        // 此处end不会被比较交换,不会打乱已排序的顺序
+        int pivot = a [end];
         int i = start;
         int temp;
         for (int j = start; j < end; j++) {
-            if (a[j] < pivot) { // 将小数提前
-                temp = a[j];
-                a[j] = a[i];
-                a[i] = temp;
+            // 将小数提前, 游标后移,
+            // 游标永远等于或大于中间值,
+            // 若游标等于中间值,则只能说明,中间值之前的数全都小于中间值
+            // 游标一直移动,最后交换的为本身
+            if (a[j] < pivot) {
+                if (i != j) { //  如果 i == j 移动游标,去除本身交换
+                    temp = a[j];
+                    a[j] = a[i];
+                    a[i] = temp;
+                }
                 i++;
             }
         }
-        temp = a[end];  // 交换游标与中间值
-        a[end] = a[i];
-        a[i] = temp;
+        if (i != end) {
+            temp = a[end];  // 交换游标与中间值
+            a[end] = a[i];
+            a[i] = temp;
+        }
         return i;
     }
 }
