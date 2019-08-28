3427107ce48323b298d57d54d91032b4ee457b8a
yujiang <yujiang4j@qq.com>
2019-08-28 14:55:46 +0800

    Add algorithm MergeSortedArray/RotateString from lintCode 6/8

---
+++ src/algorithm/merge_sorted_array/MergeSortedArray.java
@@ -0,0 +1,59 @@
+package algorithm.merge_sorted_array;
+
+import java.util.Arrays;
+
+/**
+ * 合并两个数组， 皆有序
+ */
+public class MergeSortedArray {
+
+    public static void main(String[] args) {
+        int[] a = {1,3,5};
+        int[] b = {2,4};
+        int[] result = mergeSortedArray(a, b);
+        System.out.println(Arrays.toString(result));
+
+    }
+
+    private static int[] mergeSortedArray(int[] A, int[] B) {
+        int aLength = A.length;
+        int bLength = B.length;
+        int[] result = new int[aLength + bLength];
+        int aPointer = 0, bPointer = 0, index = 0;
+        int aValue, bValue;
+        do {
+            if (aPointer < aLength) {
+                aValue = A[aPointer];
+            }else {
+                aValue = 0;
+            }
+            if (bPointer < bLength) {
+                bValue = B[bPointer];
+            }else {
+                bValue = 0;
+            }
+            if (aValue == 0) {
+                if (bValue == 0) {
+                    break;
+                }else{
+                    result[index++] = bValue;
+                    bPointer++;
+                    continue;
+                }
+            }
+            if (bValue == 0) {
+                result[index++] = aValue;
+                aPointer++;
+                continue;
+            }
+            if (aValue <= bValue) {
+                result [index++] = aValue;
+                aPointer++;
+            }else{
+                result[index++] = bValue;
+                bPointer++;
+            }
+        } while (aPointer < aLength || bPointer < bLength);
+        return result;
+    }
+}
---
+++ src/algorithm/rotate_string/RotateString.java
@@ -0,0 +1,40 @@
+package algorithm.rotate_string;
+
+import org.junit.Test;
+
+/**
+ * 按偏移量旋转字符串
+ * 借用思想 插入排序
+ * 原地排序
+ */
+public class RotateString {
+
+    public static void main(String [] args) {
+        String str = new String("abcdefg");
+        char[] arrStr = str.toCharArray();
+        solution(arrStr, 10);
+        String result = new String(arrStr);
+        System.out.println(result);
+    }
+
+
+    private static void solution (char[] str, int offset) {
+        int len = str.length;
+        if (len == 0){
+            return;
+        }
+        if (offset >= len) {
+            offset = offset % len;
+        }
+        int j;
+        char value;
+        for (int i = len - offset ; i < len ; i++) {
+            value = str[i];
+            j = i - 1;
+            for (int moveLen = len - offset ; moveLen > 0 ; j-- , moveLen-- ) {
+                str[j + 1] = str[j];
+            }
+            str[j + 1] = value;
+        }
+    }
+}
